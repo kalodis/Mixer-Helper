a7d53ad72410c5af59654d0f73e073344a8d8b99
Gwardo420 <torreykimbrough@gmail.com>
2019-10-23 15:09:52 -0400

    Advertisements only post to channels that are online on the GAL

--- commands/spam.js
+++ commands/spam.js
@@ -24,20 +24,24 @@ request.get('https://mixer-helper.s3.amazonaws.com/spams.json', (err, res, body)
 })
 
 messageSpams = async () => {
-  for (let token of mbot.spams) {
-    let channel = await mbot.getChannel(token)
-    let message = spamMessages[Math.floor(Math.random() * spamMessages.length)]
-    if (token != mbot.user.channel.token) {
-      if (mbot.chats[token]) {
-        mbot.chats[token].msg(message).catch(err => console.log(err))
-      } else {
-        let chat = await mbot.getChat(channel.id)
-        chat = await mbot.join(channel, chat)
-        mbot.chats[token].msg(message).catch(err => console.log(err))
-      }
-      console.log('\x1b[34m%s\x1b[0m', `[ADVERTISEMENT] Sent Advertisement to ${token}`)
-    } else console.log('\x1b[34m%s\x1b[0m', `[ADVERTISEMENT] ${token} is offline - Ad not sent!`)
-    if (mbot.spams.length >= 1000) await mbot.delay(3500)
+  try {
+    for (let token of mbot.spams) {
+      let channel = await mbot.getChannel(token)
+      let message = spamMessages[Math.floor(Math.random() * spamMessages.length)]
+      if (channel.online && token != mbot.user.channel.token) {
+        if (mbot.chats[token]) {
+          mbot.chats[token].msg(message).catch
+        } else {
+          let chat = await mbot.getChat(channel.id)
+          chat = await mbot.join(channel, chat)
+          mbot.chats[token].msg(message).catch
+        }
+        console.log('\x1b[34m%s\x1b[0m', `[ADVERTISEMENT] Sent Advertisement to ${token}`)
+      } else console.log('\x1b[34m%s\x1b[0m', `[ADVERTISEMENT] ${token} is offline - Ad not sent!`)
+      if (mbot.spams.length >= 1000) await mbot.delay(3500)
+    }
+  } catch(err){
+  console.log(err)
   }
 }
 setInterval(messageSpams, 1000 * 60 * spamInterval)
